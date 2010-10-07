f86ab3e76df62ee3273248332e427e868e2a00dc
Michael Ziegler <diese-addy@funzt-halt.net>
2010-10-07 09:10:04 +0200

    add lockfile (nagios runs the process multiple times)

--- py/switchports.py
+++ py/switchports.py
@@ -1,9 +1,62 @@
 #!/usr/bin/env python
 
+import os
+import errno
 import re
 import sys
 import serial
+import time
+import stat
 from time import sleep
+
+MAX_WAIT = 10
+LOCKFILE = "/var/lock/ttyS0.lock"
+
+while True:
+    try:
+        fd = os.open(LOCKFILE, os.O_EXCL | os.O_RDWR | os.O_CREAT)
+        # we created the LOCKFILE, so we're the owner
+        break
+
+    except OSError, e:
+        if e.errno != errno.EEXIST:
+            # should not occur
+            raise
+
+        try:
+            # the lock file exists, try to stat it to get its age
+            # and read it's contents to report the owner PID
+            f = open(LOCKFILE, "r")
+            s = os.stat(LOCKFILE)
+
+        except OSError, e:
+            if e.errno != errno.EEXIST:
+                sys.exit("%s exists but stat() failed: %s" %
+                         (LOCKFILE, e.strerror))
+            # we didn't create the LOCKFILE, so it did exist, but it's
+            # gone now. Just try again
+            continue
+        
+        # we didn't create the LOCKFILE and it's still there, check
+        # its age
+        now = int(time.time())
+        if now - s[stat.ST_MTIME] > MAX_WAIT:
+
+            pid = f.readline()
+            sys.exit("%s has been locked for more than " \
+                     "%d seconds (PID %s)" % (LOCKFILE, MAX_WAIT,
+                     pid))
+
+
+        # it's not been locked too long, wait a while and retry
+        f.close()
+        time.sleep(1)
+
+f = os.fdopen(fd, "w")
+
+f.write("%d\n" % os.getpid())
+f.close()
+
 
 # The switch will answer:
 #  Port    Admin     Mode  Speed Duplex  Bandwidth  Flow control    Link
@@ -16,45 +69,49 @@ regex = r"\s*(?P<port>\d{1,2}):\s+(?P<admin>(?:en|dis)abled)\s+(?P<mode>\w+)\s+(
 ser = serial.Serial("/dev/ttyS0", timeout=1)
 
 for char in "port show\r\n":
-	ser.write(char)
-	sleep(.01)
-	
+    ser.write(char)
+    sleep(.01)
+    
 ports = []
 currline = ""
 
 cped = re.compile(regex)
 while True:
-	data = ser.read(1)
-	if not data:
-		break
-	if data in "\r\n":
-		mm = cped.match(currline)
-		if mm:
-			ports.append({
-				'port':    int(mm.group("port")),
-				'admin':   mm.group("admin").lower() == "enabled",
-				'mode':    mm.group("mode"),
-				'speed':   int(mm.group("speed")) if mm.group("speed") != "N/A" else None,
-				'duplex':  { 'N/A': None, 'Full': True, 'Half': False }[mm.group("duplex")],
-				'flow_control': mm.group('flow_control') == "enabled",
-				'link':    mm.group("link") == "Up",
-				})
-		currline = ""
-	else:
-		currline += data
+    data = ser.read(1)
+    if not data:
+        break
+    if data in "\r\n":
+        mm = cped.match(currline)
+        if mm:
+            ports.append({
+                'port':    int(mm.group("port")),
+                'admin':   mm.group("admin").lower() == "enabled",
+                'mode':    mm.group("mode"),
+                'speed':   int(mm.group("speed")) if mm.group("speed") != "N/A" else None,
+                'duplex':  { 'N/A': None, 'Full': True, 'Half': False }[mm.group("duplex")],
+                'flow_control': mm.group('flow_control') == "enabled",
+                'link':    mm.group("link") == "Up",
+                })
+        currline = ""
+    else:
+        currline += data
 
 ser.close()
+os.unlink(LOCKFILE)
 
 if len(sys.argv) == 2:
-	portno = int(sys.argv[1])
-	for pp in ports:
-		if pp['port'] == portno:
-			print pp['link']
+    portno = int(sys.argv[1])
+    for pp in ports:
+        if pp['port'] == portno:
+            print pp['link']
+            sys.exit( {True: 0, False: 2}[pp['link']] )
 else:
-	pcount = 0
+    pcount = 0
 
-	for pp in ports:
-		if pp['link']:
-			pcount += 1
+    for pp in ports:
+        if pp['link']:
+            pcount += 1
 
-	print "ports.value", pcount
+    print "ports.value", pcount
+
+
