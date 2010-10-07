542d23abb468f6e6b03ee8cec88cdac7ff3265c7
Michael Ziegler <diese-addy@funzt-halt.net>
2010-10-07 09:30:38 +0200

    only query necessary ports

--- py/switchports.py
+++ py/switchports.py
@@ -66,52 +66,63 @@ f.close()
 
 regex = r"\s*(?P<port>\d{1,2}):\s+(?P<admin>(?:en|dis)abled)\s+(?P<mode>\w+)\s+(?P<speed>N/A|\d+)\s+(?P<duplex>Full|Half|N/A)\s+(?P<bandwidth>\w+)\s+(?P<flow_control>\w+)\s+(?P<link>Down|Up).*"
 
-ser = serial.Serial("/dev/ttyS0", timeout=1)
+def queryports(ser, qryports=None):
+    if qryports is None:
+        qryports = []
 
-for char in "port show\r\n":
-    ser.write(char)
-    sleep(.01)
-    
-ports = []
-currline = ""
+    for char in ("port show %s\r\n" % ','.join([str(pn) for pn in qryports])):
+        ser.write(char)
+        sleep(.01)
+        
+    ports = []
+    currline = ""
 
-cped = re.compile(regex)
-while True:
-    data = ser.read(1)
-    if not data:
-        break
-    if data in "\r\n":
-        mm = cped.match(currline)
-        if mm:
-            ports.append({
-                'port':    int(mm.group("port")),
-                'admin':   mm.group("admin").lower() == "enabled",
-                'mode':    mm.group("mode"),
-                'speed':   int(mm.group("speed")) if mm.group("speed") != "N/A" else None,
-                'duplex':  { 'N/A': None, 'Full': True, 'Half': False }[mm.group("duplex")],
-                'flow_control': mm.group('flow_control') == "enabled",
-                'link':    mm.group("link") == "Up",
-                })
-        currline = ""
-    else:
-        currline += data
-
-ser.close()
-os.unlink(LOCKFILE)
+    cped = re.compile(regex)
+    while True:
+        data = ser.read(1)
+        if not data:
+            break
+        if data in "\r\n":
+            mm = cped.match(currline)
+            if mm:
+                ports.append({
+                    'port':    int(mm.group("port")),
+                    'admin':   mm.group("admin").lower() == "enabled",
+                    'mode':    mm.group("mode"),
+                    'speed':   int(mm.group("speed")) if mm.group("speed") != "N/A" else None,
+                    'duplex':  { 'N/A': None, 'Full': True, 'Half': False }[mm.group("duplex")],
+                    'flow_control': mm.group('flow_control') == "enabled",
+                    'link':    mm.group("link") == "Up",
+                    })
+            currline = ""
+        else:
+            currline += data
+    return ports
 
 if len(sys.argv) == 2:
     portno = int(sys.argv[1])
+    ser = serial.Serial("/dev/ttyS0", timeout=1)
+    try:
+        ports = queryports( ser, [portno] )
+    finally:
+        ser.close()
+        os.unlink(LOCKFILE)
     for pp in ports:
         if pp['port'] == portno:
             print pp['link']
             sys.exit( {True: 0, False: 2}[pp['link']] )
 else:
     pcount = 0
+    ser = serial.Serial("/dev/ttyS0", timeout=1)
+    try:
+        ports = queryports(ser)
+    finally:
+        ser.close()
+        os.unlink(LOCKFILE)
 
     for pp in ports:
         if pp['link']:
             pcount += 1
 
     print "ports.value", pcount
-
 
