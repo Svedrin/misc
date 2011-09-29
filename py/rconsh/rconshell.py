683c16e699611e32247250780e8ff285feb9f2e1
Michael Ziegler <diese-addy@funzt-halt.net>
2011-09-29 20:11:32 +0200

    add copyright stuff

--- py/rconsh/rconshell.py
+++ py/rconsh/rconshell.py
@@ -1,6 +1,27 @@
 # -*- coding: utf-8 -*-
 # kate: space-indent on; indent-width 4; replace-tabs on;
 
+"""
+ *  An RCon shell for Linux, comparable to HLSW. It connects to the
+ *  game server and waits for you to enter commands, which are then
+ *  executed on the server via RCon. Meanwhile, the server logs are
+ *  being displayed (if enabled).
+ *
+ *  Based on SRCDSpy by Christopher Munn.
+ *
+ *  Copyright © 2011, Michael "Svedrin" Ziegler <diese-addy@funzt-halt.net>
+ *
+ *  rconshell.py is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This package is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+"""
+
 import sys
 import threading
 import readline
