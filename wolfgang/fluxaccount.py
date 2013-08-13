dae1ec1698389f026ef367eefd181902c4882c84
Michael Ziegler <diese-addy@funzt-halt.net>
2013-08-13 18:32:20 +0200

    if requests isn't available, try falling back to the old httplib code, even though it sucks ass and doesn't support proxies

--- wolfgang/fluxaccount.py
+++ wolfgang/fluxaccount.py
@@ -2,11 +2,46 @@
 # kate: space-indent on; indent-width 4; replace-tabs on;
 
 import json
-import requests
 
 from urlparse import urljoin, urlparse
 
 from wolfobject import WolfObject
+
+try:
+    import requests
+except ImportError:
+    import os
+    import httplib
+    import base64
+    if "http_proxy" in os.environ:
+        raise
+
+    from wolfgang.prettyprint import colorprint, Colors
+    colorprint(Colors.red, "The requests module is not available. You don't seem to require a proxy, so I'm gonnna work around this. If you do, you need to install requests.")
+
+    class ReqResponse(object):
+        def __init__(self, status_code, text):
+            self.status_code = status_code
+            self.text = text
+
+    class requests(object):
+        @classmethod
+        def post(cls, url, data, auth, headers):
+            purl = urlparse( url )
+            conn = {
+                "http":  httplib.HTTPConnection,
+                "https": httplib.HTTPSConnection
+                }[purl.scheme.lower()]( purl.netloc )
+            conn.putrequest( "POST", purl.path )
+            for key, val in headers.items():
+                conn.putheader( key, val )
+            auth = base64.encodestring(':'.join(auth)).replace('\n', '')
+            conn.putheader( "Authorization", "Basic %s" % auth )
+            conn.endheaders()
+            conn.send(data)
+            resp = conn.getresponse()
+            return ReqResponse(resp.status, resp.read())
+
 
 class FluxAccount(WolfObject):
     objtype = "fluxaccount"
