30d05fc97fd4e1f849a5ba1dbcfbda42a4370b48
Michael Ziegler <diese-addy@funzt-halt.net>
2013-08-23 20:43:12 +0200

    add an extremely basic view to update the display field

--- display/views.py
+++ display/views.py
@@ -1,1 +1,16 @@
-# Create your views here.
+# -*- coding: utf-8 -*-
+# kate: space-indent on; indent-width 4; replace-tabs on;
+
+import json
+
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from monitoring.models import Check
+
+@csrf_exempt
+def set_display(request, app, obj):
+    cc = Check.objects.get(id=request.POST["id"])
+    cc.display = request.POST["display"]
+    cc.save()
+    return HttpResponse( json.dumps({"display": cc.display}), mimetype="application/json" )
