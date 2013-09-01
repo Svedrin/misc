09b2274bc3d4363f1b58c45065b3c6d5290d597e
Michael Ziegler <diese-addy@funzt-halt.net>
2013-09-01 21:08:41 +0200

    add graph zoom buttonés

--- fluxmon/settings.py
+++ fluxmon/settings.py
@@ -1,4 +1,5 @@
 # Django settings for fluxmon project.
+# kate: space-indent on; indent-width 4; replace-tabs on;
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -92,6 +93,17 @@ TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
 #     'django.template.loaders.eggs.Loader',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.contrib.messages.context_processors.messages',
+    'django.core.context_processors.request',
 )
 
 MIDDLEWARE_CLASSES = (
