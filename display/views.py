81a685062e18cd2b3b61cb9b61575076e3719302
Michael Ziegler <diese-addy@funzt-halt.net>
2014-04-08 20:04:25 +0200

    looks like I refactored this thing somehow, but I can't remember jack

--- display/views.py
+++ display/views.py
@@ -2,6 +2,7 @@
 # kate: space-indent on; indent-width 4; replace-tabs on;
 
 import json
+import logging
 
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
@@ -16,19 +17,35 @@ from display.models    import ItemDisplay
 def set_display(request, app, obj):
     content_type = ContentType.objects.get(app_label=app, model=obj)
 
-    # TODO: use ItemDisplay.objects.update_or_create()
-    display, created = ItemDisplay.objects.get_or_create(
-        content_type = content_type,
-        object_id    = int(request.POST["id"]),
-        defaults     = {"display": request.POST["display"]}
-        )
-    if not created:
-        display.display = request.POST["display"]
-        display.save()
+    if request.POST["display"]:
+        # TODO: use ItemDisplay.objects.update_or_create()
+        logging.debug("setting display")
+        display, created = ItemDisplay.objects.get_or_create(
+            content_type = content_type,
+            object_id    = int(request.POST["id"]),
+            defaults     = {"display": request.POST["display"]}
+            )
+        if not created:
+            display.display = request.POST["display"]
+            display.save()
+    else:
+        try:
+            display = ItemDisplay.objects.get(
+                content_type = content_type,
+                object_id    = int(request.POST["id"]),
+                )
+        except ItemDisplay.DoesNotExist:
+            logging.debug("display is not set")
+            display = None
+        else:
+            logging.debug("deleting display")
+            display.delete()
 
     # make sure the old field is cleared (will be removed soon)
-    if hasattr(display.content_object, "display"):
+    if display is not None and hasattr(display.content_object, "display"):
         display.content_object.display = ''
         display.content_object.save()
 
-    return HttpResponse( json.dumps({"display": display.display}), mimetype="application/json" )
+    return HttpResponse( json.dumps({
+        "display": display.display if display is not None else ""
+    }), mimetype="application/json" )
