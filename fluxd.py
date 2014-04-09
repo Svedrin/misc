2a4f47745a0e31caf590ad2988aa217fc8864eca
Michael Ziegler <diese-addy@funzt-halt.net>
2014-04-09 23:02:27 +0200

    port fluxd to the logging module

--- fluxd.py
+++ fluxd.py
@@ -4,6 +4,7 @@
 
 import sys
 import socket
+import logging
 import os.path
 
 from time     import time, sleep
@@ -22,11 +23,22 @@ def main():
     parser.add_option("-i", "--interval", default=300, type="int")
     parser.add_option("-f", "--fqdn",     default=socket.getfqdn(), type="string", help=("FQDN to use (defaults to %s)" % socket.getfqdn()))
     parser.add_option("-n", "--noop",     default=False, action="store_true", help="Only detect checks and exit, don't commit or run them")
+    parser.add_option("-v", "--verbose",  default=0, action="count", help="Increase verbosity")
 
     options, posargs = parser.parse_args()
     if posargs:
         print "I don't take any positional arguments (see -h for help)."
         return 3
+
+    rootlogger = logging.getLogger()
+    rootlogger.name = "fluxd"
+    rootlogger.setLevel(logging.DEBUG)
+
+    if options.verbose:
+        logch = logging.StreamHandler()
+        logch.setLevel({2: logging.DEBUG, 1: logging.INFO, 0: logging.WARNING}[int(options.verbose)])
+        logch.setFormatter( logging.Formatter('%(asctime)s - %(levelname)s - %(message)s') )
+        rootlogger.addHandler(logch)
 
     wc = WolfConfig(options.config)
     wc.parse()
@@ -47,13 +59,13 @@ def main():
     myhost = wc.objects[myhostname]
 
     account = wc.find_objects_by_type("fluxaccount")[0]
-    print "Using account %s." % account.name
+    logging.info("Using account %s." % account.name)
 
     # Discover checkable objects
     new_checks = []
     for sensortype in SensorMeta.sensortypes:
         if sensortype not in wc.objects:
-            print "Sensor type '%s' is installed but unknown to the config, skipped." % sensortype
+            logging.warning("Sensor type '%s' is installed but unknown to the config, skipped." % sensortype)
             continue
 
         sensor = SensorMeta.sensortypes[sensortype](wc)
@@ -70,12 +82,12 @@ def main():
                 params.update(target_params)
                 checks = wc.find_objects_by_params("check", **params)
                 if not checks:
-                    print "Found new target on %s: %s" % (confobj.name, target_params)
+                    logging.warning( "Found new target on %s: %s" % (confobj.name, target_params) )
                     check = wc.add_object("check", confobj.name + ",".join(target_params.values()), [], params)
                     params["uuid"] = check["uuid"]
                     new_checks.append(params)
                 else:
-                    print "Found known target on %s: %s" % (confobj.name, target_params)
+                    logging.info( "Found known target on %s: %s" % (confobj.name, target_params) )
 
     if options.noop:
         return "Check discovery finished but no-op is active, exiting."
