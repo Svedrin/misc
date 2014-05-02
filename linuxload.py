8f5b9328c4b9c4cb7673b8506181f77495a6572f
Michael Ziegler <diese-addy@funzt-halt.net>
2014-05-02 18:59:16 +0200

    add uptime and linuxload checks

---
+++ linuxload.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# kate: space-indent on; indent-width 4; replace-tabs on;
+
+import os
+
+from sensors.sensor import AbstractSensor
+
+class LinuxLoadSensor(AbstractSensor):
+    def discover(self, target):
+        return [{}]
+
+    def check(self, checkinst):
+        return dict(zip(("load1", "load5", "load15"), (float(secs) for secs in open("/proc/loadavg").read().strip().split()[:3]))), {}
