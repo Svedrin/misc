d97a67f0652961305ee67e91a41454092a0ff1bc
Michael Ziegler <diese-addy@funzt-halt.net>
2014-05-25 17:28:11 +0200

    add sensor to query SMAspot

---
+++ smaspot.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# kate: space-indent on; indent-width 4; replace-tabs on;
+
+import re
+import os
+import subprocess
+
+from sensors.sensor import AbstractSensor
+
+class SmaSpotSensor(AbstractSensor):
+    def discover(self, target):
+        return []
+
+    def check(self, checkinst):
+        proc = subprocess.Popen(
+                   ["/root/smaspot/SMAspot/bin/Release/SMAspot", "-v2", "-nocsv"],
+                   stdout=subprocess.PIPE, stderr=subprocess.STDOUT
+                )
+        out, err = proc.communicate()
+
+        ret = {}
+
+        wantlines = ("EToday", "ETotal", "Feed-In Time", "String", "Phase", "Total Pac")
+        for line in out.split("\n"):
+            line = line.strip()
+            for wantline in wantlines:
+                if line.startswith(wantline):
+                    break
+            else:
+                continue
+
+            values = re.findall(r'(?P<field>\w+)\s*:\s+(?P<value>\d+\.\d+)(?P<unit>\w*)', line)
+
+            phase = re.findall(r'(String|Phase) (?P<phase>\d+)', line)
+            if phase:
+                phase = phase[0][1]
+            else:
+                phase = ''
+
+            for field, valstr, unit in values:
+                if unit.startswith("k"):
+                    mult = 1000
+                else:
+                    mult = 1
+
+                ret[field.lower() + phase] = float(valstr) * mult
+
+        return ret, {}
