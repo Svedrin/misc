3c5fb425311ca43ac9a9960b258366d64142cea2
Michael Ziegler <diese-addy@funzt-halt.net>
2014-06-23 20:07:18 +0200

    add host creation form

--- hosts/forms.py
+++ hosts/forms.py
@@ -6,5 +6,13 @@ from django.conf   import settings
 from django.forms  import Form
 from django.utils.translation import ugettext_lazy as _, ugettext
 
+from mptt.forms    import TreeNodeChoiceField
+
+from hosts.models  import Domain
+
 class ConfirmHostDeleteForm( Form ):
     fqdn    = forms.CharField(help_text=_("Please enter the FQDN of the host you wish to delete to confirm."))
+
+class AddHostForm( Form ):
+    fqdn    = forms.CharField(help_text=_("host.somedomain.com"))
+    pubkey  = forms.CharField(help_text=_("the contents of .keys/id_rsa_4096.pub"), widget=forms.Textarea())
--- hosts/views.py
+++ hosts/views.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 # kate: space-indent on; indent-width 4; replace-tabs on;
 
+import uuid
+
 from django.shortcuts               import render_to_response, get_object_or_404, get_list_or_404
 from django.http                    import Http404, HttpResponse, HttpResponseRedirect
 from django.template                import RequestContext
@@ -9,7 +11,8 @@ from django.contrib.auth.views      import redirect_to_login
 from django.core.urlresolvers       import reverse
 
 from hosts.models import Domain, Host
-from hosts.forms  import ConfirmHostDeleteForm
+from hosts.forms  import ConfirmHostDeleteForm, AddHostForm
+from msgsign.models import PublicKey
 
 @login_required
 def domains(request):
@@ -48,3 +51,31 @@ def delete_host(request, fqdn):
         'exec_checks':   host.check_exec_set.filter(is_active=True).exclude(target_host=host)
         }, context_instance=RequestContext(request))
 
+@login_required
+def add_host(request, domain):
+    domain = get_object_or_404(Domain, id=domain)
+    if not domain.has_perm(request.user, "c", target_model=Host):
+        return redirect_to_login(request.build_absolute_uri())
+    if request.method == "POST":
+        postdata = request.POST.copy()
+        if '.' not in postdata["fqdn"]:
+            postdata["fqdn"] = "%s.%s" % (postdata["fqdn"], domain)
+        if postdata["fqdn"][-1] != ".":
+            postdata["fqdn"] += "."
+        form = AddHostForm( postdata )
+        if form.is_valid():
+            thehost = Host.objects.create(fqdn=postdata["fqdn"], domain=domain)
+            pubkey  = PublicKey.objects.create(
+                        owner     = request.user,
+                        host      = thehost,
+                        uuid      = str(uuid.uuid4()),
+                        publickey = postdata["pubkey"])
+            return HttpResponseRedirect(reverse(host, args=(thehost.fqdn,)))
+    else:
+        form = AddHostForm()
+        form.fields["fqdn"].help_text="hostname.%s" % domain
+
+    return render_to_response("hosts/hostadd.html", {
+        'domain': domain,
+        'form': form,
+        }, context_instance=RequestContext(request))
