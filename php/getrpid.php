e12a621ff8b64078e090d6b5da904264de9b31e6
Michael Ziegler <diese-addy@funzt-halt.net>
2015-06-19 23:00:01 +0200

    drop aastra support, make the name message timeout after 5 seconds so we can hangup again, add dictionary

--- php/getrpid.php
+++ php/getrpid.php
@@ -5,13 +5,13 @@
 // Requires a contacts directory created by zsync. See also:
 // https://bitbucket.org/Svedrin/misc/src/tip/py/zsync.py
 //
-// aastra URI: outgoing = http://hive.local.lan/getrpid.php?phone=aastra&cid=$$REMOTENUMBER$$
-// snom URI:   outgoing = http://hive.local.lan/getrpid.php?phone=snom&cid=$remote&
+// aastra URI: outgoing = http://hive.local.lan/getrpid.php?cid=$$REMOTENUMBER$$
+// snom URI:   outgoing = http://hive.local.lan/getrpid.php?cid=$remote&
 
 error_reporting(E_ALL);
 
 function unify_number($number){
-    $number = str_replace(" ", "", $number); // Android likes to put those in
+    $number = str_replace([" ", "-"], "", $number); // Android likes to put those in
     if( strpos($number, "00") === 0 )
         return "+".substr($number, 2);
     if( strpos($number, "0") === 0 )
@@ -37,32 +37,61 @@ function find_callerid($number){
     return $number;
 }
 
-function format_aastra($callerid){
-    $output = "<AastraIPPhoneFormattedTextScreen Timeout=\"30\">\n";
-    $output.= "<Line>{$callerid}</Line>\n";
-    $output.= "</AastraIPPhoneFormattedTextScreen>\n";
-    return $output;
+$output = '<?xml version="1.0" encoding="UTF-8"?>';
+
+switch($_GET["action"]){
+    case 'exit':
+        $output.= '<exit />';
+        break;
+
+    case 'directory':
+        $contacts = json_decode(file_get_contents("/var/lib/asterisk/.zsync/contacts.json"), true);
+
+        $output.= '<SnomIPPhoneDirectory speedselect="select">';
+        $output.= '  <Title>Directory</Title>';
+        $output.= '  <Prompt>Dial</Prompt>';
+
+        foreach( $contacts as $contactinfo ){
+            foreach( ["cellular_telephone_number", "business2_telephone_number", "business_telephone_number", "home_telephone_number", "home2_telephone_number"] as $field ){
+                if( isset($contactinfo["props"][$field])){
+                    $number = unify_number($contactinfo["props"][$field]);
+                    $shortnum = substr($number, 3, 5);
+
+                    if( strpos($_SERVER["HTTP_USER_AGENT"], "snom300-SIP") !== FALSE ){
+                        $splitname = explode(', ', $contactinfo["props"]["fileas"]);
+                        $initial = substr($splitname[1], 0, 1);
+                        $contactname = "{$splitname[0]}, {$initial}. (0{$shortnum}…)";
+                    }
+                    else{
+                        $contactname = "{$contactinfo["props"]["fileas"]} (0{$shortnum}…)";
+                    }
+
+                    $output.= '  <DirectoryEntry>';
+                    $output.= '    <Name>'.$contactname.'</Name>';
+                    $output.= '    <Telephone>'.$number.'</Telephone>';
+                    $output.= '  </DirectoryEntry>';
+                }
+            }
+        }
+        $output.= '</SnomIPPhoneDirectory>';
+        break;
+
+    default:
+        $callerid = htmlspecialchars(find_callerid($_GET["cid"]));
+
+        if( strpos($_SERVER["HTTP_USER_AGENT"], "snom300-SIP") !== FALSE ){
+            if( mb_strlen($callerid) > 14 && strpos($callerid, ',') !== FALSE ){
+                $callerid = implode(",<br/>", explode(', ', $callerid));
+            }
+        }
+
+        $output.= "<SnomIPPhoneText>";
+        $output.= "<Text>{$callerid}</Text>";
+        $output.= '<fetch mil="5000">http://'.$_SERVER["HTTP_HOST"].$_SERVER['SCRIPT_NAME'].'?action=exit</fetch>';
+        $output.= "</SnomIPPhoneText>";
+        break;
 }
 
-function format_snom($callerid){
-    if( mb_strlen($callerid) > 14 && strpos($callerid, ',') !== FALSE ){
-        $callerid = implode(",\n", explode(', ', $callerid));
-    }
-    $output = "<SnomIPPhoneText>\n";
-    $output.= "<Text>{$callerid}</Text>\n";
-    $output.= "</SnomIPPhoneText>\n";
-    return $output;
-}
-
-// error_log("Looking up callerid for {$_GET["cid"]}");
-$callerid = htmlspecialchars(find_callerid($_GET["cid"]));
-
-if( $_GET["phone"] == "aastra" ){
-    $output = format_aastra($callerid);
-}
-else{
-    $output = format_snom($callerid);
-}
 
 header("Content-Type: text/xml");
 header("Content-Length: ".strlen($output));
