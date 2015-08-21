bacc41bb633892351299d064ecc9b63e1a2c8c6c
Michael Ziegler <diese-addy@funzt-halt.net>
2015-08-21 21:53:07 +0200

    call the controller for zoom

--- static/interactiveGraphCtrl.js
+++ static/interactiveGraphCtrl.js
@@ -21,6 +21,15 @@ fluxmon.controller("InteractiveGraphCtrl", function($scope, GraphDataService){
             timezone: 'browser'
         },
     };
+
+    $scope.zoomIn = function(){
+        alert("in");
+    }
+
+    $scope.zoomOut = function(){
+        alert("out");
+    }
+
     $scope.$watchGroup(['check_uuid', 'variable_name'], function(){
         if( $scope.check_uuid && $scope.variable_name ){
             GraphDataService.get_data($scope.check_uuid, $scope.variable_name).then(function(result){
@@ -39,20 +48,24 @@ fluxmon.controller("InteractiveGraphCtrl", function($scope, GraphDataService){
 fluxmon.directive("zoom", function(){
     return {
         restrict: "A",
-        scope: false,
+        scope: false, // Trying to properly isolate this scope would break the <flot> directive
         link: function(scope, element, attr){
             var mc = new Hammer.Manager($(element).children("div")[0]);
             var pinch = new Hammer.Pinch();
             mc.add([pinch]);
             mc.on("pinchin", function(ev) {
-                alert("OMFG IN");
+                scope.zoomIn({});
             });
             mc.on("pinchout", function(ev) {
-                alert("OMFG OUT");
+                scope.zoomOut({});
             });
             $(element).children("div").bind("wheel", function(ev){
-                // -3 = upwards, +3 = downwards
-                console.log(ev.originalEvent.deltaY);
+                if(ev.originalEvent.deltaY < 0){
+                    scope.zoomIn({});
+                }
+                else{
+                    scope.zoomOut({});
+                }
             });
         }
     };
