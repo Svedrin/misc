315de010e737e43d9832296661bdb06e6f6d0f30
Michael Ziegler <diese-addy@funzt-halt.net>
2015-09-01 23:50:30 +0200

    remove obsolete display app

--- display/admin.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# kate: space-indent on; indent-width 4; replace-tabs on;
-
-from django.contrib import admin
-
-from display.models import ItemDisplay
-
-admin.site.register( ItemDisplay )
--- fluxmon/settings.py
+++ fluxmon/settings.py
@@ -148,7 +148,6 @@ INSTALLED_APPS = (
     'hosts',
     'fluxacl',
     'monitoring',
-    'display',
     'msgsign',
 )
 
