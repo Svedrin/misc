f10223de28a3f9d97fb49c27aa917fa8a16cf4c0
Michael Ziegler <diese-addy@funzt-halt.net>
2015-09-01 23:54:46 +0200

    remove obsolete forms.pys

--- monitoring/forms.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# kate: space-indent on; indent-width 4; replace-tabs on;
-
-from django import forms
-
-from monitoring.models import Sensor, SensorVariable, Check
-
-class SearchForm(forms.Form):
-    query = forms.CharField()
