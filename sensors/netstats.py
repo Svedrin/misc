a76df09b982630659831a599cf447832d46eab1e
Michael Ziegler <diese-addy@funzt-halt.net>
2015-09-09 21:40:08 +0200

    add SSH transport to the netstats plugin

--- sensors/netstats.py
+++ sensors/netstats.py
@@ -2,6 +2,7 @@
 # kate: space-indent on; indent-width 4; replace-tabs on;
 
 import os
+
 from time import time, mktime
 from datetime import datetime, timedelta
 
@@ -11,9 +12,69 @@ from sensors.sensor import AbstractSensor
 from sensors.values import ValueDict
 
 class NetstatsSensor(AbstractSensor):
+    def __init__(self, conf):
+        AbstractSensor.__init__(self, conf)
+        self._cache     = {}
+        self._cachetime = {}
+
+    def _get_via_ssh(self, target):
+        try:
+            import spur
+        except ImportError:
+            import logging
+            logging.error("cannot import spur, SSH is not available")
+            return {}
+
+        # retrieve data via SSH by running grep to collect the info we need, then convert it into a dict such as:
+        # {
+        #   "uptime": 1337,
+        #   "eth0":   { stats },
+        #   "wlan0":  { stats }
+        # }
+
+        with spur.SshShell(hostname=target.name, username=target["ssh_username"], password=target["ssh_password"]) as sh:
+            grepresult = sh.run(["sh", "-c", "grep . /proc/uptime /sys/class/net/*/statistics/* /sys/class/net/*/speed"], allow_error=True)
+
+        result = {}
+
+        for line in grepresult.output.split("\n"):
+            line = line.strip()
+            if not line:
+                continue
+
+            fpath, fcontent = line.split(":")
+
+            if fpath == "/proc/uptime":
+                result["uptime"] = float(fcontent.split()[0])
+
+            elif "/statistics/" in fpath:
+                _, _, _, _, iface, _, statkey = fpath.split("/")
+                if iface not in result:
+                    result[iface] = {}
+                result[iface][statkey] = int(fcontent)
+
+            elif "/speed" in fpath:
+                _, _, _, _, iface, _ = fpath.split("/")
+                if iface not in result:
+                    result[iface] = {}
+                result[iface]["speed"] = int(fcontent)
+
+        return result
+
+    def cache(self, target):
+        if target.name not in self._cache or time() - self._cachetime[target] > 280:
+            self._cache[target.name]     = self._get_via_ssh(target)
+            self._cachetime[target.name] = time()
+        return self._cache[target.name]
+
     def discover(self, target):
         if target.name != self.conf.environ["fqdn"]:
-            return []
+            if "ssh_username" not in target or "ssh_password" not in target:
+                return []
+            return [ {"interface": dev}
+                for dev in self.cache(target)
+                if dev != "uptime"
+            ]
         ctx = Context()
         return [ {"interface": dev["INTERFACE"]}
             for dev in ctx.list_devices()
@@ -33,31 +94,46 @@ class NetstatsSensor(AbstractSensor):
         storetime, storedata = self._load_store(checkinst["uuid"])
         havestate = (storedata is not None)
 
-        ctx = Context()
-        dev = Device.from_name(ctx, "net", checkinst["interface"])
+        # let's see if we need to do a remote check
+        if checkinst.target.name != self.conf.environ["fqdn"]:
+            data   = self.cache(checkinst.target)
+            uptime = timedelta(seconds=data["uptime"])
+            speed  = data[checkinst["interface"]].get("speed", None)
+            if speed:
+                speed *= 125000 # MBit/s -> bytes/s
 
-        if hasattr(dev, "time_since_initialized") and dev.time_since_initialized:
-            uptime = dev.time_since_initialized
+            currstate = ValueDict(data[checkinst["interface"]])
+            if "speed" in currstate:
+                del currstate["speed"]
+
+            currstate["timestamp"] = self._cachetime[checkinst.target.name]
+
         else:
-            with open( "/proc/uptime" ) as ut:
-                uptime = timedelta(seconds=float(ut.read().split()[0]))
+            ctx = Context()
+            dev = Device.from_name(ctx, "net", checkinst["interface"])
+
+            if hasattr(dev, "time_since_initialized") and dev.time_since_initialized:
+                uptime = dev.time_since_initialized
+            else:
+                with open( "/proc/uptime" ) as ut:
+                    uptime = timedelta(seconds=float(ut.read().split()[0]))
+
+            try:
+                speed = int(dev.attributes["speed"]) * 125000 # MBit/s -> bytes/s
+            except (KeyError, ValueError):
+                speed = None
+
+            statfields = os.listdir(dev.sys_path + "/statistics")
+
+            currstate = ValueDict(zip(statfields, [
+                int(dev.attributes["statistics/%s" % statfield]) for statfield in statfields
+            ]))
+            currstate["timestamp"] = time()
 
         createstamp = int(mktime((datetime.now() - uptime).timetuple()))
 
         if havestate and ("device" not in storedata or "initialized" not in storedata["device"] or createstamp != storedata["device"]["initialized"]):
             havestate = False
-
-        try:
-            speed = int(dev.attributes["speed"]) * 125000
-        except (KeyError, ValueError):
-            speed = None
-
-        statfields = os.listdir(dev.sys_path + "/statistics")
-
-        currstate = ValueDict(zip(statfields, [
-            int(dev.attributes["statistics/%s" % statfield]) for statfield in statfields
-        ]))
-        currstate["timestamp"] = time()
 
         if havestate:
             diff = currstate - storedata["state"]
