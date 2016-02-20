165eeef5c74632eb4ee5b287c2bfc1dc468c1b4b
Michael Ziegler <diese-addy@funzt-halt.net>
2016-02-20 23:54:29 +0100

    add OpAmp'ed reading, convert everything that's calculated to double

--- arduino/TemperatureSensor.ino
+++ arduino/TemperatureSensor.ino
@@ -4,6 +4,7 @@ unsigned long resistance[] = {562, 617, 677, 740, 807, 877, 951, 990, 1029, 1111
 unsigned int  num_steps    = sizeof(centigrade)/sizeof(int);
 
 unsigned long rvcc = 5000;
+unsigned long rref = 1200;
 unsigned long vref = 1200;
 unsigned long vcc  =    0;
 
@@ -13,22 +14,31 @@ void setup() {
   Serial.begin(9600);
 }
 
-float toCentigrade(unsigned long x){
-  // Convert ADC units to a voltage
-  unsigned long vin = vcc * x / 1024;
+double toVoltsSimple(unsigned long x){
+  // Convert ADC units to a voltage, without any amplification stuff.
+  return vcc * x / 1024.0;
+}
 
+double toVoltsWithOpAmp(unsigned long x){
+  // Convert ADC units to a voltage, reading amplified data.
+  // See https://de.wikipedia.org/wiki/Operationsverst%C3%A4rker#Nichtinvertierender_Verst.C3.A4rker_.28Elektrometerverst.C3.A4rker.29
+  // The voltage we're reading is the input voltage amplified by v = (1 + rvcc / rref).
+  return toVoltsSimple(x) / (1 + (rvcc / (double)rref));
+}
+
+double toCentigrade(unsigned long vin){
   // Find the interval we're in
-  unsigned long vupper;
-  unsigned long vlower;
+  double vupper;
+  double vlower;
   unsigned long rgnd;
   int idx;
   for( idx = 1; idx < num_steps; idx++ ){
     // vupper is the voltage at the upper boundary, vlower the one at the lower boundary.
     rgnd = resistance[idx];
-    vupper = vcc * rgnd / (rvcc + rgnd);
+    vupper = vcc * rgnd / (double)(rvcc + rgnd);
 
     rgnd = resistance[idx - 1];
-    vlower = vcc * rgnd / (rvcc + rgnd);
+    vlower = vcc * rgnd / (double)(rvcc + rgnd);
 
     if(vlower <= vin && vin < vupper){
       break;
@@ -36,9 +46,9 @@ float toCentigrade(unsigned long x){
   }
 
   // We're in [idx - 1:idx]. Interpolate where exactly
-  float partial = (vin - vlower) / (float)(vupper - vlower);
-  int   cupper = centigrade[idx];
-  int   clower = centigrade[idx - 1];
+  double partial = (vin - vlower) / (vupper - vlower);
+  int    cupper = centigrade[idx];
+  int    clower = centigrade[idx - 1];
   return clower + (partial * (cupper - clower));
 }
 
@@ -49,16 +59,32 @@ void readTemp(int analogInPin){
   Serial.print("sensor = " );
   Serial.print(sensorValue);
   Serial.print("\t temp = ");
-  Serial.print(toCentigrade(sensorValue));
+  Serial.print(toCentigrade(toVoltsSimple(sensorValue)));
+}
+
+void readTempTheCoolWay(int analogInPin){
+  unsigned long sensorValue = analogRead(analogInPin);
+  double vin = toVoltsWithOpAmp(sensorValue);
+
+  // print the results to the serial monitor:
+  Serial.print("sensor = " );
+  Serial.print(sensorValue);
+  Serial.print("\t volts = ");
+  Serial.print(vin);
+  Serial.print("\t temp = ");
+  Serial.print(toCentigrade(vin));
 }
 
 void loop() {
-  vcc = vref * 1024 / analogRead(pinref);
+  vcc = vref * 1024.0 / analogRead(pinref);
 
   readTemp(A0);
   Serial.print("\t");
 
   readTemp(A1);
+  Serial.print("\t");
+
+  readTempTheCoolWay(A2);
   Serial.println();
 
   delay(200);
