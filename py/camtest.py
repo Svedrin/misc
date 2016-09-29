f1b4c7f54ed03c0263d3d9441a6eb5ac6c2551f3
Michael Ziegler <diese-addy@funzt-halt.net>
2016-09-29 23:28:04 +0200

    add corner crosshairs and commentary

--- py/camtest.py
+++ py/camtest.py
@@ -24,8 +24,10 @@ screen = pygame.display.set_mode((1920, 1080), pygame.HWSURFACE | pygame.DOUBLEB
 font = pygame.font.Font(None, 60)
 
 while True:
+    # capture an image from the camera and put it directly on screen
     surface_img = cam.get_image(screen)
 
+    # inner crosshair
     pygame.draw.line(
         screen, (255, 0, 0, 255),
         (1920/2 - 50, 1080/2),
@@ -37,12 +39,47 @@ while True:
         (1920/2, 1080/2 + 50),
         1)
 
+    # corners:
+    # upper left
+    pygame.draw.lines(
+        screen, (255, 0, 0, 255), False,
+        ( (1920 / 2 - 500, 1080/2 - 450),
+          (1920 / 2 - 500, 1080/2 - 500),
+          (1920 / 2 - 450, 1080/2 - 500)),
+        2)
+
+    # upper right
+    pygame.draw.lines(
+        screen, (255, 0, 0, 255), False,
+        ( (1920 / 2 + 500, 1080/2 - 450),
+          (1920 / 2 + 500, 1080/2 - 500),
+          (1920 / 2 + 450, 1080/2 - 500)),
+        2)
+
+    # lower left
+    pygame.draw.lines(
+        screen, (255, 0, 0, 255), False,
+        ( (1920 / 2 - 500, 1080/2 + 450),
+          (1920 / 2 - 500, 1080/2 + 500),
+          (1920 / 2 - 450, 1080/2 + 500)),
+        2)
+
+    # lower right
+    pygame.draw.lines(
+        screen, (255, 0, 0, 255), False,
+        ( (1920 / 2 + 500, 1080/2 + 450),
+          (1920 / 2 + 500, 1080/2 + 500),
+          (1920 / 2 + 450, 1080/2 + 500)),
+        2)
+
+    # convert the image into a PIL image so we can work with it
     pil_string_image = pygame.image.tostring(surface_img, "RGBA", False)
     im = Image.frombytes("RGBA", (1920, 1080), pil_string_image)
     sw = im.convert("L")
     contrast = ImageEnhance.Contrast(sw)
     sw = contrast.enhance(5)
 
+    # try to find a dark object in the middle
     thresh = 150
     min_x = None
     max_x = None
@@ -70,6 +107,7 @@ while True:
                     max_y = y
 
     if min_x and min_y and max_x and max_y:
+        # found something -> draw a blue rect around it
         pygame.draw.lines(
             screen, (0, 0, 255, 255), True,
             ((min_x, min_y),
@@ -78,7 +116,7 @@ while True:
              (max_x, min_y)),
             2)
 
-
+        # count the amount of dark pixels in the area
         swdata = numpy.asarray( sw.crop((min_x, min_y, max_x, max_y)) )
         dark_pixels = (swdata < thresh).sum()
 
@@ -87,11 +125,13 @@ while True:
             0, (0, 255, 0, 255), (0, 0, 0, 0)
         )
     else:
+        # nothing found -> indicate it
         prozentes = font.render(
             '??.??%',
             0, (0, 255, 0, 255), (0, 0, 0, 0)
         )
     screen.blit(prozentes, (10, 10))
 
+    # update the screen and sleep a bit
     pygame.display.flip()
     sleep(1 / 30.)
