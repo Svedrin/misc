9ef9ee8f4fcba9c386928cbd7dd04bd852f1f93b
Michael Ziegler <diese-addy@funzt-halt.net>
2016-09-29 21:44:24 +0200

    add image detection tests

---
+++ py/camtest.py
@@ -0,0 +1,89 @@
+
+import sys
+
+import pygame
+import pygame.camera
+import pygame.display
+import pygame.draw
+import pygame.image
+
+from PIL import Image, ImageEnhance, ImageDraw
+from time import sleep
+
+pygame.init()
+
+pygame.camera.init()
+cam = pygame.camera.Camera("/dev/video1", (1920, 1080) )
+cam.start()
+
+pygame.display.init()
+screen = pygame.display.set_mode((1920, 1080))
+
+while True:
+    surface_img = cam.get_image()
+    screen.blit(surface_img, (0, 0))
+
+    pil_string_image = pygame.image.tostring(surface_img, "RGBA", False)
+    im = Image.frombytes("RGBA", (1920, 1080), pil_string_image)
+
+    sw = im.convert("LA")
+
+    contrast = ImageEnhance.Contrast(sw)
+
+    sw = contrast.enhance(5)
+
+    #draw = ImageDraw.Draw(sw)
+
+    thresh = 150
+    min_x = None
+    max_x = None
+    min_y = None
+    max_y = None
+
+    for x in range( 1920 / 2 - 500, 1920 / 2 + 500, 10 ):
+        px, alpha = sw.getpixel((x, 1080/2))
+        if min_x is None:
+            if px <= thresh:
+                min_x = x
+        else:
+            if max_x is None:
+                if px > thresh:
+                    max_x = x
+
+    for y in range( 1080 / 2 - 500, 1080 / 2 + 500, 10 ):
+        px, alpha = sw.getpixel((1920/2, y))
+        if min_y is None:
+            if px <= thresh:
+                min_y = y
+        else:
+            if max_y is None:
+                if px > thresh:
+                    max_y = y
+
+    print min_x, min_y, max_x, max_y
+
+    if min_x and min_y and max_x and max_y:
+        #draw.rectangle( (min_x, min_y, max_x, max_y) )
+        pygame.draw.lines(
+            screen, (0, 0, 255, 255), True,
+            ((min_x, min_y),
+             (min_x, max_y),
+             (max_x, max_y),
+             (max_x, min_y)),
+            2)
+
+        dark_pixels = 0
+
+        for x in range(min_x, max_x):
+            for y in range(min_y, max_y):
+                px, alpha = sw.getpixel((x, y))
+                if px < thresh:
+                    dark_pixels += 1
+
+        print "Dark pixels:", dark_pixels
+        print "In %:", dark_pixels / ( (max_x - min_x) * float(max_y - min_y) ) * 100.
+    else:
+        print "nix gefunden"
+
+    pygame.display.flip()
+    sleep(0.001)
