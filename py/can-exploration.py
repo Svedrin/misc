ae2f4c0237cc577da8a1a88eb6f81294266873fe
Michael Ziegler <diese-addy@funzt-halt.net>
2016-11-27 14:08:16 +0100

    implement message transfer

--- py/can-exploration.py
+++ py/can-exploration.py
@@ -58,10 +58,10 @@ def cpu1():
     current_tick = 0
 
     send_queue = [
-        (    3,    "hallo" ),
-        (   17,    "huhu"  ),
-        (   25,    "jaho" ),
-        (    3,    "hallo" ),
+        (    3, "hallo".ljust(8, '-') ),
+        (   17, "huhu".ljust(8, '-')  ),
+        (   25, "jaho".ljust(8, '-')  ),
+        (    3, "hallo".ljust(8, '-') ),
         ]
     message    = send_queue[0]
     sending_id = True
@@ -92,8 +92,24 @@ def cpu1():
                     if my_bit and not bus.data:    # someone else killed our bit
                         outq.put("cpu1 is a sad panda and waits for the next frame.")
                 else:
-                    sending_id = False         # ID is complete
-                    outq.put("cpu1 has completed sending ID %d" % message[0])
+                    if sending_id:
+                        sending_id = False         # ID is complete
+                        outq.put("cpu1 has completed sending ID %d" % message[0])
+
+                    if body_pos < 8 * 8:
+                        # the message can be sent unconditionally
+                        body_char_idx = body_pos // 8
+                        body_bit_idx  = body_pos  % 8
+                        body_pos += 1
+                        if body_char_idx < 8:
+                            bus.data = bool(ord(message[1][body_char_idx]) & (1<<body_bit_idx))
+                            outq.put("cpu1 sent a %s" % bus.data)
+                            sleep(0.5)
+                            # recv part is empty
+                        else:
+                            message = None
+                            start_at_tick = None
+                            outq.put("cpu1 message complete")
 
         lastclock = bus.clock
         sleep(0.1)
@@ -106,6 +122,11 @@ def cpu2():
 
     start_at_tick = None
     revd_id = 0
+    recving_id = True
+
+    revd_message = ""
+    recv_buf = 0
+    recv_buf_pos = 0
 
     while not bus.shutdown:
         if bus.clock and not lastclock:    # _/¯
@@ -119,14 +140,32 @@ def cpu2():
                 start_at_tick = current_tick
 
             if start_at_tick is not None:
+                # send part is empty
                 sleep(0.5)
+
+                outq.put("cpu2 received a %s" % bus.data)
 
                 current_bit_idx = 11 - (current_tick - start_at_tick)
                 if current_bit_idx >= 0:
                     revd_id |= (bus.data<<current_bit_idx)
-                    outq.put("cpu2 received a %s" % bus.data)
                 else:
-                    outq.put("cpu2 has completed receiving ID %s" % revd_id)
+                    if recving_id:
+                        recving_id = False
+                        outq.put("cpu2 has completed receiving ID %s" % revd_id)
+
+                    recv_buf |= (bus.data<<recv_buf_pos)
+                    recv_buf_pos += 1
+
+                    if recv_buf_pos == 8:
+                        revd_message += chr(recv_buf)
+                        recv_buf = 0
+                        recv_buf_pos = 0
+
+                        outq.put("cpu2 is receiving a message: %s" % revd_message)
+                        if len(revd_message) == 8:
+                            outq.put("cpu2 message is complete: %s" % revd_message)
+                            revd_message = ""
+
 
         lastclock = bus.clock
         sleep(0.1)
