0ed4797439b07af67551b52f0ac0bbcc7dbea6f9
Michael Ziegler <diese-addy@funzt-halt.net>
2017-07-30 01:14:40 +0200

    we now can format posts into emails

--- rust/rss2imap/src/main.rs
+++ rust/rss2imap/src/main.rs
@@ -1,14 +1,20 @@
 extern crate crossbeam;
+extern crate hyper;
 extern crate imap;
+extern crate mime_multipart;
 extern crate openssl;
 extern crate rss;
+extern crate sha1;
 extern crate yaml_rust;
 
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::io::{Cursor,SeekFrom};
+use hyper::header::Headers;
 use rss::Channel;
 use imap::client::Client;
+use mime_multipart::{Node,Part,write_multipart,generate_boundary};
 use openssl::ssl::{SslConnectorBuilder, SslMethod};
 use yaml_rust::{Yaml,YamlLoader};
 
@@ -21,6 +27,89 @@ r#"<div style="background-color: #ededed; border: 1px solid grey; margin: 5px;">
 </div>
 {content}"#;
 
+
+fn hash_id(item: &rss::Item) -> Option<String> {
+    if let Some(guid) = item.guid() {
+        let sha = &mut sha1::Sha1::new();
+        sha.update(guid.value().as_bytes());
+        Some(sha.digest().to_string())
+    } else {
+        None
+    }
+}
+
+fn build_headers(headers: Vec<(String, String)>) -> Headers {
+    let mut hdrs = Headers::new();
+    for (header, value) in headers {
+        hdrs.append_raw(header, value.into_bytes());
+    }
+    return hdrs;
+}
+
+fn build_email(feedname: &str, item: &rss::Item) -> Result<String, String> {
+    /**
+     * MIME Structure:
+     *
+     *  multipart/related
+     *  + multipart/alternative
+     *  | + text/html
+     *  + image/png
+     *  + image/jpg
+     *  + ...
+     *
+     * The multipart/alternative seems superfluous, but (at least)
+     * Thunderbird doesn't render the email correctly without it.
+     */
+
+    let node = Node::Multipart((build_headers(vec![
+        (
+            "Content-Type".to_string(),
+            format!("multipart/related; boundary=\"{}\"",
+                String::from_utf8(generate_boundary())
+                    .map_err(|err| err.to_string())?).to_string()
+        ),
+        ("From".to_string(),          feedname.to_string()),
+        ("Subject".to_string(),       item.title().unwrap_or("no title").to_string()),
+        ("X-RSS2IMAP-ID".to_string(), hash_id(item).unwrap_or("no ID".to_string())),
+        ("Date".to_string(),          "TODO".to_string()),
+    ]), vec![
+        Node::Multipart((build_headers(vec![
+            (
+                "Content-Type".to_string(),
+                format!("multipart/alternative; boundary=\"{}\"",
+                        String::from_utf8(generate_boundary())
+                            .map_err(|err| err.to_string())?).to_string()
+            )
+        ]), vec![
+            Node::Part(Part {
+                headers: build_headers(vec![
+                    ("Content-Type".to_string(), "text/html; charset=\"utf-8\"".to_string())
+                ]),
+                body: CONTENT_TEMPLATE
+                    .replace("{feed}",    feedname)
+                    .replace("{title}",   item.title().unwrap_or("no title"))
+                    .replace("{link}",    item.link().unwrap_or("no link"))
+                    .replace("{content}", item.description().unwrap_or("no content"))
+                    .into_bytes()
+            } )
+        ]))
+    ]));
+
+    let mut stream = Cursor::new(vec![0; 4096]);
+    let mut result = String::new();
+    write_multipart(&mut stream, &vec![], &vec![node])
+        .map_err(|err| err.to_string())?;
+    // write_multipart puts in one boundary too many, so strip leading --\r\n by seeking to 4, ...
+    stream.seek(SeekFrom::Start(4))
+        .map_err(|err| err.to_string())?;
+    stream.read_to_string(&mut result)
+        .map_err(|err| err.to_string())?;
+    Ok(
+        result.trim_right_matches('\0')
+            .trim_right_matches('-')      // ... and strip trailing ---- by using trim.
+            .to_string()
+    )
+}
 
 fn run() -> Result<(), String> {
     let conffile = &mut String::new();
@@ -100,18 +189,13 @@ fn run() -> Result<(), String> {
                 let channel = match Channel::from_url(&feedurl) {
                     Ok(chan) => chan,
                     Err(err) => {
-                        println!("Error parsing feed {}: {}", feedname, err);
+                        println!("{} -> {} failed to parse: {}", dirname, feedname, err);
                         return;
                     }
                 };
                 for item in channel.items().iter() {
-                    println!("{:?} -> {:?}", dirname,
-                        CONTENT_TEMPLATE
-                            .replace("{feed}",    &feedname)
-                            .replace("{title}",   item.title().unwrap_or("no title"))
-                            .replace("{link}",    item.link().unwrap_or("no link"))
-                            .replace("{content}", item.description().unwrap_or("no content"))
-                    );
+                    println!("{} -> {}: {:?}", dirname, feedname,
+                        build_email(&feedname, item).unwrap());
                     return;
                 }
             });
