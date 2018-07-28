47a9644ebb14957a91a6b631764a18c1f1522ffe
Michael Ziegler <diese-addy@funzt-halt.net>
2018-07-28 18:59:51 +0200

    simplify mydht to not listen for incoming messages (we're configurable somewhat easily via mqtt_configure, so probably not necessary)

--- esp/mydht.py
+++ esp/mydht.py
@@ -20,42 +20,25 @@ def mqtt_configure():
             "password": password
         }, fd)
 
-def mqtt_cb(*args):
-    print("Recv!", args)
-
 def main():
     client_id = ubinascii.hexlify(machine.unique_id())
     with open("mqtt.json", "rb") as fd:
         conf = json.load(fd)
 
     cl = mqtt.MQTTClient(client_id, conf["broker"], user=conf["username"], password=conf["password"])
-    cl.set_callback(mqtt_cb)
     cl.connect()
     i = 0
 
-    cl.subscribe("outTopic")
-    cl.sock.settimeout(1)
-
-    last_msmt = 0
-
     d = dht.DHT22(machine.Pin(4))
     while True:
-        now = time.time()
-        if now > last_msmt + 5:
-            d.measure()
-            print("boop!", i)
-            cl.publish("sensor/%s/temperature" % conf["sensor"], str(d.temperature()))
-            cl.publish("sensor/%s/humidity"    % conf["sensor"], str(d.humidity()))
-            cl.publish("sensor/%s/i"           % conf["sensor"], str(i))
-            i += 1
-            last_msmt = now + 5
-        try:
-            cl.wait_msg()
-        except OSError as err:
-            if err.args[0] == 110:
-                continue
-            else:
-                raise
+        d.measure()
+        print("boop!", i)
+        cl.publish("sensor/%s/temperature" % conf["sensor"], str(d.temperature()))
+        cl.publish("sensor/%s/humidity"    % conf["sensor"], str(d.humidity()))
+        cl.publish("sensor/%s/i"           % conf["sensor"], str(i))
+        i += 1
+        cl.check_msg()
+        time.sleep(5)
 
 
-# curl("http://192.168.0.168/mydht.py", "mydht.py", 8000)
+# curl("http://192.168.0.168:8000/mydht.py", "mydht.py")
