cc56db3d64b5148d506db538b5fcd9f61a994c8e
Michael Ziegler <michael.ziegler@proemion.com>
2021-05-10 22:47:55 +0200

    safeguard against EXIF data failures

--- sh/imgsort.sh
+++ sh/imgsort.sh
@@ -9,6 +9,10 @@ for image in "$@"; do
     datestr="$(exiv2 -g Exif.Image.DateTime -Pv "$image")"
     year="$(cut -d: -f1 <<<$datestr)"
     mnth="$(cut -d: -f2 <<<$datestr)"
+    if [ -z "$datestr" ] || [ -z "$year" ] || [ -z "$mnth" ]; then
+        echo "$image: failed to read date from EXIF data" >&2
+        continue
+    fi
     mkdir -p $year/$mnth
     echo mv $image $year/$mnth/
     mv $image $year/$mnth/
