f1853f0ad3b8a12d61e16d7b18efd0f985bcef3c
Michael Ziegler <michael.ziegler@rhoencloud.de>
2024-04-30 09:37:43 +0200

    handle EOFError and KeyboardInterrupt

--- py/rsa.py
+++ py/rsa.py
@@ -5,65 +5,69 @@
 #
 # <http://de.wikipedia.org/wiki/RSA-Kryptosystem#Verfahren>
 
-print("Wir brauchen zwei Primzahlen p und q. 13 und 89 funktionieren beispielsweise.")
-print("Primzahlen bis 100: 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97.")
-print()
+def main():
+    print("Wir brauchen zwei Primzahlen p und q. 13 und 89 funktionieren beispielsweise.")
+    print("Primzahlen bis 100: 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97.")
+    print()
 
-p = int( input( "p: " ) )
-q = int( input( "q: " ) )
+    p = int( input( "p: " ) )
+    q = int( input( "q: " ) )
 
-n     = p * q
-phi_n = (p - 1) * (q - 1)
+    n     = p * q
+    phi_n = (p - 1) * (q - 1)
 
-print("RSA-Modul: N = %d - φ(N) = %d" % ( n, phi_n ))
+    print("RSA-Modul: N = %d - φ(N) = %d" % ( n, phi_n ))
 
-print("Suche mögliche Schlüsselpaare:")
+    print("Suche mögliche Schlüsselpaare:")
 
 
-keypairs = []
+    keypairs = []
 
-for e in range( 1, (phi_n + 1), 2 ):
-    d = (phi_n + 1.0)/e
-    if d == int(d):
-        # konvertiere float nach int. Wichtig weil float in der Größe der Zahlen
-        # limitiert ist, int in Python aber nicht. Wenn wir die Zahlen als float
-        # belassen würden, führt dies zu Overflows bei den Potenzierungen.
-        d = int(d)
+    for e in range( 1, (phi_n + 1), 2 ):
+        d = (phi_n + 1.0)/e
+        if d == int(d):
+            # konvertiere float nach int. Wichtig weil float in der Größe der Zahlen
+            # limitiert ist, int in Python aber nicht. Wenn wir die Zahlen als float
+            # belassen würden, führt dies zu Overflows bei den Potenzierungen.
+            d = int(d)
 
-        # kurze Prüfung dass dieses Schlüsselpaar funktioniert
-        if (( ((123 ** e) % n) ** d) % n) != 123:
-            continue
+            # kurze Prüfung dass dieses Schlüsselpaar funktioniert
+            if (( ((123 ** e) % n) ** d) % n) != 123:
+                continue
 
-        janein = input( "e = %d, d = %d -- dieses Schlüsselpaar benutzen? [jN] " % ( e, d ) )
+            janein = input( "e = %d, d = %d -- dieses Schlüsselpaar benutzen? [jN] " % ( e, d ) )
 
-        if janein == 'j':
-            pubkey, privkey = e, d
+            if janein == 'j':
+                pubkey, privkey = e, d
+                break
+    else:
+        raise ValueError("Alle Möglichkeiten erschöpft")
+
+    print()
+    print("Öffentlicher Schlüssel = (%d,%d)" % ( pubkey,  n ))
+    print("Geheimer Schlüssel     = (%d,%d)" % ( privkey, n ))
+    print()
+
+    while(True):
+        msg = input( "Nachricht:     " )
+        if not msg:
             break
-else:
-    raise ValueError("Alle Möglichkeiten erschöpft")
 
-print()
-print("Öffentlicher Schlüssel = (%d,%d)" % ( pubkey,  n ))
-print("Geheimer Schlüssel     = (%d,%d)" % ( privkey, n ))
-print()
+        ciph = [ ((ord(char) ** pubkey) % n) for char in msg ]
 
-while(True):
-    msg = input( "Nachricht:     " )
-    if not msg:
-        break
+        print("Verschlüsselt:", ciph)
 
-    ciph = [ ((ord(char) ** pubkey) % n) for char in msg ]
+        deciph = ""
+        for char in ciph:
+            deciph += chr((char ** privkey) % n)
 
-    print("Verschlüsselt:", ciph)
-
-    deciph = ""
-    for char in ciph:
-        deciph += chr((char ** privkey) % n)
-
-    print("Entschlüsselt:", deciph)
-    print()
-    print()
+        print("Entschlüsselt:", deciph)
+        print()
+        print()
 
 
-
-
+if __name__ == '__main__':
+    try:
+        main()
+    except (EOFError, KeyboardInterrupt):
+        pass
