58c84411a4414aeccc64ba37d8b438b98f0aab84
Michael Ziegler <diese-addy@funzt-halt.net>
2025-01-16 12:08:17 +0100

    add missing $

--- sh/embiggen-disks.sh
+++ sh/embiggen-disks.sh
@@ -7,7 +7,7 @@ set -e
 set -u
 
 DISKS="$(cd /sys/class/block; echo [svx]d?)"
-for DISK in DISKS; do
+for DISK in $DISKS; do
     echo 1 > "/sys/class/block/$DISK/device/rescan"
     parted "/dev/$DISK" resizepart 1 100%
     if pvdisplay "/dev/${DISK}1" &> /dev/null; then
